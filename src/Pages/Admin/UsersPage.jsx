045b7d27844206a0cee82ca9232da4e1910ab8cb
mirza2018 <mirza.eee.4th@gmail.com>
2025-03-17 08:20:59 +0600

    add all

---
+++ src/Pages/Admin/UsersPage.jsx
@@ -0,0 +1,156 @@
+import { useMemo } from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import axios from "axios";
+import { ConfigProvider, Input } from "antd";
+import { SearchOutlined } from "@ant-design/icons";
+import { IoMdAddCircleOutline } from "react-icons/io";
+
+//* Modal Table
+import AllUserTable from "../../Components/Tables/Admin/AllUserTable";
+import ViewUserModal from "../../Components/Modal/Admin/ViewUserModal";
+import BlockUserModal from "../../Components/Modal/Admin/BlockUserModal";
+import AddCategoriesModal from "../../Components/Modal/Admin/AddCategoriesModal";
+
+const UsersPage = () => {
+  //* Store Search Value
+  const [searchText, setSearchText] = useState("");
+
+  //* Use to set user
+  const [data, setData] = useState([]);
+
+  const [loading, setLoading] = useState(true);
+
+  //* It's Use to Show Modal
+  const [isCompanyViewModalVisible, setIsCompanyViewModalVisible] =
+    useState(false);
+
+  //* It's Use to Block Modal
+  const [isCompanyBlockModalVisible, setIsCompanyBlockModalVisible] =
+    useState(false);
+
+  //* It's Use to Add Modal
+  const [isAddCompanyModalVisible, setIsAddCompanyModalVisible] =
+    useState(false);
+
+  //* It's Use to Set Seclected User to Block and view
+  const [currentCompanyRecord, setCurrentCompanyRecord] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("/data/userData.json");
+        setData(response?.data); // Make sure this is an array
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const filteredCompanyData = useMemo(() => {
+    if (!searchText) return data;
+    return data.filter((item) =>
+      item.userName.toLowerCase().includes(searchText.toLowerCase())
+    );
+  }, [data, searchText]);
+
+  const onSearch = (value) => {
+    setSearchText(value);
+  };
+
+  const showAddCompanyModal = () => {
+    setIsAddCompanyModalVisible(true);
+  };
+
+  const showCompanyViewModal = (record) => {
+    setCurrentCompanyRecord(record);
+    setIsCompanyViewModalVisible(true);
+  };
+
+  const showCompanyBlockModal = (record) => {
+    setCurrentCompanyRecord(record);
+    setIsCompanyBlockModalVisible(true);
+  };
+
+  const handleCancel = () => {
+    setIsCompanyViewModalVisible(false);
+    setIsCompanyBlockModalVisible(false);
+    setIsAddCompanyModalVisible(false);
+  };
+
+  const handleCompanyBlock = (data) => {
+    console.log("Blocked Company:", {
+      id: data?.id,
+      companyName: data?.companyName,
+    });
+    setIsCompanyViewModalVisible(false);
+    setIsCompanyBlockModalVisible(false);
+  };
+
+  return (
+    <div
+      className="bg-highlight-color min-h-[90vh]  rounded-xl"
+      style={{ boxShadow: "0px 0px 5px  rgba(0, 0, 0, 0.25)" }}
+    >
+      {/* Header  */}
+      <div className="bg-secondary-color w-full p-4   rounded-tl-xl rounded-tr-xl">
+        <div className=" w-[95%] mx-auto  flex items-center justify-between">
+          <p className="text-3xl text-primary-color font-semibold">
+            Users List
+          </p>
+          <div className="flex gap-4 items-center">
+            <ConfigProvider
+              theme={{ token: { colorTextPlaceholder: "#f3f3f3" } }}
+            >
+              <Input
+                placeholder="Search User..."
+                value={searchText}
+                onChange={(e) => onSearch(e.target.value)}
+                className="font-semibold !border-primary-color !placeholder:text-secondary-color !bg-white text-secondary-color py-2 !rounded-full"
+                prefix={
+                  <SearchOutlined className="text-secondary-color font-bold text-lg mr-2" />
+                }
+              />
+            </ConfigProvider>
+          </div>
+        </div>
+      </div>
+
+      {/* Table  */}
+      <div className="px-10 py-10">
+        <AllUserTable
+          data={filteredCompanyData}
+          loading={loading}
+          showCompanyViewModal={showCompanyViewModal}
+          showCompanyBlockModal={showCompanyBlockModal}
+          pageSize={8}
+        />
+      </div>
+
+      {/* Modals */}
+      <AddCategoriesModal
+        isAddCompanyModalVisible={isAddCompanyModalVisible}
+        handleCancel={handleCancel}
+      />
+      <ViewUserModal
+        isCompanyViewModalVisible={isCompanyViewModalVisible}
+        handleCancel={handleCancel}
+        currentCompanyRecord={currentCompanyRecord}
+        handleCompanyBlock={handleCompanyBlock}
+        showCompanyBlockModal={showCompanyBlockModal}
+      />
+      <BlockUserModal
+        isCompanyBlockModalVisible={isCompanyBlockModalVisible}
+        handleCompanyBlock={handleCompanyBlock}
+        handleCancel={handleCancel}
+        currentCompanyRecord={currentCompanyRecord}
+      />
+    </div>
+  );
+};
+
+export default UsersPage;
